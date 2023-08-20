b7cf939539d62b300fbb39692f2bd69833a448ce
RAHIDDENPOOL <ramaalborov@gmail.com>
2023-08-20 22:51:26 +0300

    fix

--- app/src/main/java/com/example/circassianrecipeapp/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/circassianrecipeapp/ui/theme/MainScreen.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.circassianrecipeapp.R
-
+//
 @Preview
 @Composable
 fun MainScreen() {
