de6c1372322fbb370ab67df9680b5c01e0bf4c53
RAHIDDENPOOL <ramaalborov@gmail.com>
2023-08-20 23:15:30 +0300

    MainScreen.kt /*TODO*/ Carousel, Bottom_App_Bar, Import Styles for text "Recipes"

--- app/src/main/java/com/example/circassianrecipeapp/view/theme/Color.kt
+++ app/src/main/java/com/example/circassianrecipeapp/view/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Custom colors
+val ThemeColorLight = Color(0xFFFAFAF3)
+val GreenColor = Color(0xFF43483F)
+val GreenSolid = Color (0xFFDFE4D7)
