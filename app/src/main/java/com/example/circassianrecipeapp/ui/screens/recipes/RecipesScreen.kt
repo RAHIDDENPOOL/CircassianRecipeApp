6850b2ae15a84cc94607f054f43c08aa9befa15e
HIDDENPOOL <ramaalborov@gmail.com>
2023-10-08 17:56:52 +0300

    RecipesScreen.kt correct

--- app/src/main/java/com/example/circassianrecipeapp/ui/screens/recipes/RecipesScreen.kt
+++ app/src/main/java/com/example/circassianrecipeapp/ui/screens/recipes/RecipesScreen.kt
@@ -3,6 +3,7 @@ package com.example.circassianrecipeapp.ui.screens.recipes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,47 +15,59 @@ import com.example.circassianrecipeapp.ui.screens.recipes.components.RecipeCards
 
 @Composable
 fun RecipesScreen(navController: NavController) {
-    TopAppBar()
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .padding(top = 55.dp),
+    MaterialTheme {
+        TopAppBar()
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 55.dp),
 
-    ) {
-        Carousel()
+        ) {
+            Carousel()
 
-        val imageId = arrayOf(
-            R.drawable.carousel_first_item,
-            R.drawable.carousel_second_item,
-            R.drawable.carousel_first_item,
-            R.drawable.carousel_second_item,
-            R.drawable.carousel_first_item,
-            R.drawable.carousel_second_item,
-        )
+            val imageId = arrayOf(
+                R.drawable.dish_one,
+                R.drawable.dish_two,
+                R.drawable.dish_one,
+                R.drawable.dish_two,
+                R.drawable.dish_one,
+                R.drawable.dish_two,
+            )
 
-        val names = arrayOf(
-            "Peperoni",
-            "Vegan",
-            "FourCheese",
-            "Margaritta",
-            "American",
-            "Mexican",
-        )
+            val names = arrayOf(
+                "Peperoni",
+                "Vegan",
+                "FourCheese",
+                "Margaritta",
+                "American",
+                "Mexican",
+            )
 
-        val ingredients = arrayOf(
-            "Tomato sos, cheese, oregano, peperoni",
-            "Tomato sos, cheese, oregano, spinach, green paprika, rukola",
-            "Tomato sos, oregano, mozzarella, goda, parmesan, cheddar",
-            "Tomato sos, cheese, oregano, bazillion",
-            "Tomato sos, cheese, oregano, green paprika, red beans",
-            "Tomato sos, cheese, oregano, corn, jalapeno, chicken",
-        )
+            val ingredients = arrayOf(
+                "Tomato sos, cheese, oregano, peperoni",
+                "Tomato sos, cheese, oregano, spinach, green paprika, rukola",
+                "Tomato sos, oregano, mozzarella, goda, parmesan, cheddar",
+                "Tomato sos, cheese, oregano, bazillion",
+                "Tomato sos, cheese, oregano, green paprika, red beans",
+                "Tomato sos, cheese, oregano, corn, jalapeno, chicken",
+            )
 
-        RecipeCardsColumn(
-            imageId = imageId,
-            names = names,
-            ingredients = ingredients,
-            navController = navController,
-        )
+            val description = arrayOf(
+                "Circassian sos, cheese, oregano, peperoni",
+                "Circassian sos, cheese, oregano, spinach, green paprika, rukola",
+                "Circassian sos, oregano, mozzarella, goda, parmesan, cheddar",
+                "Circassian sos, cheese, oregano, bazillion",
+                "Circassian sos, cheese, oregano, green paprika, red beans",
+                "Circassian sos, cheese, oregano, corn, jalapeno, chicken",
+            )
+
+            RecipeCardsColumn(
+                imageId = imageId,
+                names = names,
+                subhead = ingredients,
+                description = description,
+                navController = navController,
+            )
+        }
     }
 }
