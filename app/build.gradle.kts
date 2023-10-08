f7af9fd1fd915eaa03b0636778c36468ad8c42ab
HIDDENPOOL <110696633+RAHIDDENPOOL@users.noreply.github.com>
2023-10-08 18:02:59 +0300

    RecipesScreen&navigation (#9)
    * Верная навигационная логика
    * RecipesScreen.kt correct

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -63,8 +63,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.navigation:navigation-compose:2.7.3")
-    implementation("androidx.compose.foundation:foundation-layout-android:1.5.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.5.3")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
--- app/src/main/java/com/example/circassianrecipeapp/view/theme/Theme.kt
+++ app/src/main/java/com/example/circassianrecipeapp/view/theme/Theme.kt
@@ -15,26 +15,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+val DarkColorScheme = darkColorScheme(
     primary = TitleTextColorMaterialTheme,
     secondary = UnPickedColorMaterialTheme,
     tertiary = PickedColorMaterialTheme,
 )
 
-private val LightColorScheme = lightColorScheme(
+val LightColorScheme = lightColorScheme(
     primary = TitleTextColorMaterialTheme,
     secondary = UnPickedColorMaterialTheme,
     tertiary = PickedColorMaterialTheme,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
