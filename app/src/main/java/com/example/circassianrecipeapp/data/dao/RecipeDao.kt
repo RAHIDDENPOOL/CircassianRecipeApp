e8ae78b3eecef5dd75418afd914e33650f9ddad6
hiddenpool <ramaalborov@gmail.com>
2024-01-06 15:47:33 +0300

    fix

--- app/src/main/java/com/example/circassianrecipeapp/data/dao/RecipeDao.kt
+++ app/src/main/java/com/example/circassianrecipeapp/data/dao/RecipeDao.kt
@@ -11,16 +11,16 @@ import kotlinx.coroutines.flow.Flow
 interface RecipeDao {
 
     @Query("SELECT * FROM recipes")
-    suspend fun getRecipes(): Flow<List<Recipe>>
+    fun getRecipes(): Flow<List<Recipe>>
 
     @Query("SELECT * FROM recipes WHERE id = :recipeId")
-    suspend fun getRecipeById(recipeId: Long): Flow<Recipe?>
+    fun getRecipeById(recipeId: Long): Flow<Recipe?>
 
-    @Query("SELECT * FROM recipes ORDER BY category")
-    suspend fun getRecipesByCategory(category: String): Flow<List<Recipe>>
+    @Query("SELECT * FROM recipes ORDER BY :category")
+    fun getRecipesByCategory(category: String): Flow<List<Recipe>>
 
     @Query("SELECT * FROM recipes WHERE isFavorite = :isFavorite")
-    suspend fun getFavoriteRecipes(isFavorite: Boolean): Flow<List<Recipe>>
+    fun getFavoriteRecipes(isFavorite: Boolean): Flow<List<Recipe>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(recipe: Recipe)
